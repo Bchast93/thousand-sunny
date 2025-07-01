5eb5ccb08462933c8f6a6dffc60af42413d48674
bchast93 <bchast93@gmail.com>
2025-07-01 16:41:06 +0000

    enable apps

--- scripts/bootstrap-apps.sh
+++ scripts/bootstrap-apps.sh
@@ -5,45 +5,6 @@ source "$(dirname "${0}")/lib/common.sh"
 
 export LOG_LEVEL="debug"
 export ROOT_DIR="$(git rev-parse --show-toplevel)"
-export ROOK_DISK="/dev/nvme0n1"
-
-
-# function wipe_rook_disks() {
-#     log debug "Wiping Rook disks"
-
-#     # Skip disk wipe if Rook is detected running in the cluster
-#     # TODO: Is there a better way to detect Rook / OSDs?
-#     if kubectl --namespace rook-ceph get kustomization rook-ceph &>/dev/null; then
-#         log warn "Rook is detected running in the cluster, skipping disk wipe"
-#         return
-#     fi
-
-#     if ! nodes=$(talosctl config info --output json 2>/dev/null | jq --exit-status --raw-output '.nodes | join(" ")') || [[ -z "${nodes}" ]]; then
-#         log error "No Talos nodes found"
-#     fi
-
-#     log debug "Talos nodes discovered" "nodes=${nodes}"
-
-#     # Wipe disks on each node that match the ROOK_DISK environment variable
-#     for node in ${nodes}; do
-#         if ! disks=$(talosctl --nodes "${node}" get disk --output json 2>/dev/null \
-#             | jq --exit-status --raw-output --slurp '. | map(select(.spec.dev_path == env.ROOK_DISK) | .metadata.id) | join(" ")') || [[ -z "${nodes}" ]];
-#         then
-#             log error "No disks found" "node=${node}" "model=${ROOK_DISK}"
-#         fi
-
-#         log debug "Talos node and disk discovered" "node=${node}" "disks=${disks}"
-
-#         # Wipe each disk on the node
-#         for disk in ${disks}; do
-#             if talosctl --nodes "${node}" wipe disk "${disk}" &>/dev/null; then
-#                 log info "Disk wiped" "node=${node}" "disk=${disk}"
-#             else
-#                 log error "Failed to wipe disk" "node=${node}" "disk=${disk}"
-#             fi
-#         done
-#     done
-# }
 
 
 # Talos requires the nodes to be 'Ready=False' before applying resources
